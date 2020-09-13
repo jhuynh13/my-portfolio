e6fb3a088bb210d5b9a12c9c5a5b0460faa11b19
Johnny Huynh <johnnyhuynh28@yahoo.com>
2020-09-13 16:31:02 -0400

    Updates

---
+++ precache-manifest.ea0d52c2df46fff291eb918cb9a238ea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6579a069c22d08fdaef7d975cb055d0",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "d95f4f4388f4acaa13f3",
+    "url": "/my-portfolio/static/css/main.6199cd4c.chunk.css"
+  },
+  {
+    "revision": "1dabbe14cded04c1b132",
+    "url": "/my-portfolio/static/js/2.0f3c3eef.chunk.js"
+  },
+  {
+    "revision": "cf89bcfdc86db2c261a743301fbeb514",
+    "url": "/my-portfolio/static/js/2.0f3c3eef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d95f4f4388f4acaa13f3",
+    "url": "/my-portfolio/static/js/main.d993f5e8.chunk.js"
+  },
+  {
+    "revision": "5d4511f3fcec99c33304",
+    "url": "/my-portfolio/static/js/runtime-main.c58b4907.js"
+  },
+  {
+    "revision": "ca9faf5f4a2761358dbb1f6d4196bb2e",
+    "url": "/my-portfolio/static/media/html.ca9faf5f.jpeg"
+  },
+  {
+    "revision": "342182f69375d1c4da71742906aecfbc",
+    "url": "/my-portfolio/static/media/image1.342182f6.gif"
+  },
+  {
+    "revision": "49ceafb79494d8aad450b59bef16e7a8",
+    "url": "/my-portfolio/static/media/image3.49ceafb7.jpeg"
+  },
+  {
+    "revision": "07fb4b4ffad95c1016eda7b26f67bacc",
+    "url": "/my-portfolio/static/media/jsfullstack.07fb4b4f.jpg"
+  },
+  {
+    "revision": "155b42c91749e8a3abe18b304edc0c78",
+    "url": "/my-portfolio/static/media/mern.155b42c9.jpg"
+  },
+  {
+    "revision": "d8334acb7785f4f9f97c9f075a0871c7",
+    "url": "/my-portfolio/static/media/redux.d8334acb.jpg"
+  }
+]);
