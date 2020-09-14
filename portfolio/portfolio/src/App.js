f010a8989c46f81fd739e688bdc4efeb434f7afc
Johnny Huynh <johnnyhuynh28@yahoo.com>
2020-09-13 23:07:57 -0400

    Added projects

---
+++ portfolio/portfolio/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Route } from "react-router-dom";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import './App.css';
+import Home from './components/';
+import Portfolio from './components/Portfolio';
+import Contacts from './components/Contacts';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Route exact path="/my-portfolio" component= {Home} />
+      <Route path="/portfolio" component={Portfolio} />
+      <Route path="/contacts" component={Contacts} />
+    </>
+  );
+}
+
+export default App;
